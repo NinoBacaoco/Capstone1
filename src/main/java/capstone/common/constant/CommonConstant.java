7b2467bbf4d5000f06917f2ae7f0db567990bde2
Niño Bacaoco <Nino.bacaoco@gmail.com>
2024-06-01 18:39:04 +0800

    constants

---
+++ src/main/java/capstone/common/constant/CommonConstant.java
@@ -0,0 +1,15 @@
+package capstone.common.constant;
+
+public class CommonConstant {
+
+    public static String INVALID = "999";
+
+    public static String VALID = "000";
+
+    public static String BLANK = "";
+
+    public static String EMAIL = "mieminabua@gmail.com";
+
+    public static String NULL = null;
+
+}

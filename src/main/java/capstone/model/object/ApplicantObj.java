233f936e0441f42f922affb06592ab1870fe0054
Niño Bacaoco <Nino.bacaoco@gmail.com>
2024-06-02 02:41:51 +0800

    objects

---
+++ src/main/java/capstone/model/object/ApplicantObj.java
@@ -0,0 +1,25 @@
+package capstone.model.object;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class ApplicantObj {
+
+    // Applicant Id
+    public int applicantIdPk;
+
+    // Id
+    public String email;
+
+    public String projectTitle;
+
+    public int status;
+
+    public String university;
+
+    public int score;
+
+    public String feedback;
+}

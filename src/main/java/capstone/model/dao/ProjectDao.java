47db5c7313100dbd39f926b9a80cc22d6cbadb79
Niño Bacaoco <Nino.bacaoco@gmail.com>
2024-09-01 21:28:40 +0800

    fixed errors, modified admin home.html

--- src/main/java/capstone/model/dao/ProjectDao.java
+++ src/main/java/capstone/model/dao/ProjectDao.java
@@ -14,52 +14,52 @@ import capstone.model.dao.entity.ProjectEntity;
 @Transactional
 public interface ProjectDao extends JpaRepository<ProjectEntity, Integer> {
 
-    public final String GET_PROJECT_BY_APPLICANT_ID = "SELECT e"
-            + " FROM ProjectEntity e"
-            + " WHERE e.applicantIdPk = :applicantIdPk"
-            + " AND e.deleteFlg = false";
+        public final String GET_PROJECT_BY_APPLICANT_ID = "SELECT e"
+                        + " FROM ProjectEntity e"
+                        + " WHERE e.applicantIdPk = :applicantIdPk"
+                        + " AND e.deleteFlg = false";
 
-    public final String UPDATE_PROJECT = "UPDATE t_project "
-            + "SET "
-            + "	project_title = :projectTitle,"
-            + "	project_description = :projectDescription,"
-            + "	teams = :teams,"
-            + "	problem_statement = :problemStatement,"
-            + "	target_market = :targetMarket,"
-            + "	solution_description = :solutionDescription,"
-            + "	historical_timeline = :historicalTimeline,"
-            + "	product_service_offering = :productServiceOffering,"
-            + "	pricing_strategy = :pricingStrategy,"
-            + "	int_property_status = :intPropertyStatus,"
-            + "	objectives = :objectives,"
-            + "	scope_proposal = :scopeProposal,"
-            + "	methodology = :methodology,"
-            + "	vitae_file = :vitaeFile,"
-            + "	support_link = :supportLink "
-            + "WHERE "
-            + "	applicant_id_pk = :applicantIdPk "
-            + "AND "
-            + "	delete_flg = false";
+        public final String UPDATE_PROJECT = "UPDATE t_project "
+                        + "SET "
+                        + "	project_title = :projectTitle,"
+                        + "	project_description = :projectDescription,"
+                        + "	teams = :teams,"
+                        + "	problem_statement = :problemStatement,"
+                        + "	target_market = :targetMarket,"
+                        + "	solution_description = :solutionDescription,"
+                        + "	historical_timeline = :historicalTimeline,"
+                        + "	product_service_offering = :productServiceOffering,"
+                        + "	pricing_strategy = :pricingStrategy,"
+                        + "	int_property_status = :intPropertyStatus,"
+                        + "	objectives = :objectives,"
+                        + "	scope_proposal = :scopeProposal,"
+                        + "	methodology = :methodology,"
+                        + "	vitae_file = :vitaeFile,"
+                        + "	support_link = :supportLink "
+                        + "WHERE "
+                        + "	applicant_id_pk = :applicantIdPk "
+                        + "AND "
+                        + "	delete_flg = false";
 
-    @Modifying
-    @Query(value = UPDATE_PROJECT, nativeQuery = true)
-    public void updateProject(@Param("projectTitle") String projectTitle,
-            @Param("projectDescription") String projectDescription,
-            @Param("teams") String[] teams,
-            @Param("problemStatement") String problemStatement,
-            @Param("targetMarket") String targetMarket,
-            @Param("solutionDescription") String solutionDescription,
-            @Param("historicalTimeline") String[] historicalTimeline,
-            @Param("productServiceOffering") String[] productServiceOffering,
-            @Param("pricingStrategy") String[] pricingStrategy,
-            @Param("intPropertyStatus") String intPropertyStatus,
-            @Param("objectives") String objectives,
-            @Param("scopeProposal") String scopeProposal,
-            @Param("methodology") String methodology,
-            @Param("vitaeFile") String vitaeFile,
-            @Param("supportLink") String supportLink,
-            @Param("applicantIdPk") int applicantIdPk) throws DataAccessException;
+        @Modifying
+        @Query(value = UPDATE_PROJECT, nativeQuery = true)
+        public void updateProject(@Param("projectTitle") String projectTitle,
+                        @Param("projectDescription") String projectDescription,
+                        @Param("teams") String[] teams,
+                        @Param("problemStatement") String problemStatement,
+                        @Param("targetMarket") String targetMarket,
+                        @Param("solutionDescription") String solutionDescription,
+                        @Param("historicalTimeline") String[] historicalTimeline,
+                        @Param("productServiceOffering") String[] productServiceOffering,
+                        @Param("pricingStrategy") String[] pricingStrategy,
+                        @Param("intPropertyStatus") String intPropertyStatus,
+                        @Param("objectives") String objectives,
+                        @Param("scopeProposal") String scopeProposal,
+                        @Param("methodology") String methodology,
+                        @Param("vitaeFile") String vitaeFile,
+                        @Param("supportLink") String supportLink,
+                        @Param("applicantIdPk") int applicantIdPk) throws DataAccessException;
 
-    @Query(value = GET_PROJECT_BY_APPLICANT_ID)
-    public ProjectEntity getProjectByApplicantId(int applicantIdPk) throws DataAccessException;
+        @Query(value = GET_PROJECT_BY_APPLICANT_ID)
+        public ProjectEntity getProjectByApplicantId(int applicantIdPk) throws DataAccessException;
 }

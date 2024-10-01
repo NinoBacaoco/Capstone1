da6339193a877d6afe1e891067b804c14b8c02b8
Niño Bacaoco <Nino.bacaoco@gmail.com>
2024-10-01 13:30:28 +0800

    added functions in EmailServiceImpl, small tweaks in service folder

--- src/main/java/capstone/model/service/OfficerService.java
+++ src/main/java/capstone/model/service/OfficerService.java
@@ -7,35 +7,37 @@ import jakarta.mail.MessagingException;
 
 @Service
 public interface OfficerService {
-
-    /**
-     * To get all applicants for officer
-     * 
-     * @return OfficerInOutDto
-     */
-    public OfficerInOutDto getAllApplicants() throws Exception;
-
-    /**
-     * To accept an applicantion
-     * 
-     * @param inDto
-     * @return OfficerInOutDto
-     */
-    public void acceptApplicant(OfficerInOutDto inDto) throws MessagingException;
-
-    /**
-     * To reject an applicants
-     * 
-     * @param inDto
-     * @return OfficerInOutDto
-     */
-    public void rejectApplicant(OfficerInOutDto inDto) throws MessagingException;
-
-    /**
-     * To get the details of the application by id pk
-     * 
-     * @param inDto
-     * @return OfficerInOutDto
-     */
-    public OfficerInOutDto getApplicantDetails(OfficerInOutDto inDto);
+	
+	/**
+	 * To get the analytics details for officer dashboard
+	 * @return OfficerInOutDto
+	 */
+	public OfficerInOutDto getDetailsForOfficerDashboard();
+	
+	/**
+	 * To get all applicants for officer
+	 * @return OfficerInOutDto
+	 */
+	public OfficerInOutDto getAllApplicants() throws Exception;
+	
+	/**
+	 * To accept an applicantion
+	 * @param inDto
+	 * @return OfficerInOutDto
+	 */
+	public void acceptApplicant(OfficerInOutDto inDto) throws MessagingException;
+	
+	/**
+	 * To reject an applicants
+	 * @param inDto
+	 * @return OfficerInOutDto
+	 */
+	public void rejectApplicant(OfficerInOutDto inDto) throws MessagingException;
+	
+	/**
+	 * To get the details of the application by id pk
+	 * @param inDto
+	 * @return OfficerInOutDto
+	 */
+	public OfficerInOutDto getApplicantDetails(OfficerInOutDto inDto); 
 }
--- src/main/java/capstone/model/service/TbiBoardService.java
+++ src/main/java/capstone/model/service/TbiBoardService.java
@@ -7,26 +7,29 @@ import jakarta.mail.MessagingException;
 
 @Service
 public interface TbiBoardService {
+	
+	/**
+	 * To get analytics details for Tbi Board Dashboard
+	 * @return
+	 */
+	public TbiBoardInOutDto getDetailsForTbiBoardDashboard();
 
-    /**
-     * TO get all applicants for TbiBOard
-     * 
-     * @return ManagerInOutDto
-     */
-    public TbiBoardInOutDto getAllApplicants() throws Exception;
-
-    /**
-     * To evaluate applicant
-     * 
-     * @return
-     */
-    public void evaluateApplicant(TbiBoardInOutDto inDto) throws MessagingException;
-
-    /**
-     * To get the details of the application by id pk
-     * 
-     * @param inDto
-     * @return ApplicantInOutDto
-     */
-    public TbiBoardInOutDto getApplicantDetails(TbiBoardInOutDto inDto) throws Exception;
+	/**
+	 * TO get all applicants for TbiBOard
+	 * @return ManagerInOutDto
+	 */
+	public TbiBoardInOutDto getAllApplicants() throws Exception ;
+	
+	/**
+	 * To evaluate applicant
+	 * @return
+	 */
+	public void evaluateApplicant(TbiBoardInOutDto inDto) throws MessagingException;
+	
+	/**
+	 * To get the details of the application by id pk
+	 * @param inDto
+	 * @return ApplicantInOutDto
+	 */
+	public TbiBoardInOutDto getApplicantDetails(TbiBoardInOutDto inDto) throws Exception ; 
 }
